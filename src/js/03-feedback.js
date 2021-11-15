e5a2a27cfeddf2bded17685e6ed2ec2f326473a2
Valerian Rykov <Valerra2@gmail.com>
2021-11-15 23:16:00 +0200

    code redaction

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,40 @@
 import throttle from 'lodash.throttle';
+
 const refs = {
-  form: document.querySelector('.feedback-form'),
+  form: document.querySelector('form'),
   input: document.querySelector('input[name="email"]'),
   area: document.querySelector('textarea[name="message"]'),
 };
+const FEEDBACK_KEY = 'feedback-form-state';
 
-const FB_KEY = 'feedback-form-state';
 let value = {};
 
 refs.form.addEventListener('submit', onSubmit);
 refs.form.addEventListener('input', throttle(onText, 500));
 
-savedValue();
-
 function onSubmit(evt) {
   evt.preventDefault();
-  console.log(value);
-
   evt.currentTarget.reset();
-  localStorage.removeItem(FB_KEY);
-  value = {};
+  localStorage.removeItem(FEEDBACK_KEY);
+  console.log(value);
 }
 
-function onText(evt) {
-  if (evt.target === refs.input) {
-    value.email = evt.target.value;
+function onText(e) {
+  if (e.target === refs.input) {
+    value.email = e.target.value;
   }
-  if (evt.target === refs.area) {
-    value.message = evt.target.value;
+  if (e.target === refs.area) {
+    value.message = e.target.value;
   }
-  localStorage.setItem(FB_KEY, JSON.stringify(value));
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(value));
 }
 
-function savedValue() {
-  const savedMsg = JSON.parse(localStorage.getItem(FB_KEY));
+function fillingForm() {
+  const takeValue = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
 
-  if (savedMsg) {
-    refs.input.value = savedMsg.email;
-    refs.area.value = savedMsg.message;
+  if (takeValue) {
+    refs.input.value = takeValue.email;
+    refs.area.value = takeValue.message;
   }
 }
-form.addEventListener('submit', onSubmit);
-form.addEventListener('input', throttle(onInput, 500));
+fillingForm();
